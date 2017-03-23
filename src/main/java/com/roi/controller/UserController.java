e53345d344d5c8333a9a0d23a0e0bd543c66e5fc
Tolya <tshel231@gmail.com>
2017-03-23 14:42:13 +0300

    + mysql connector dependency

--- src/main/java/com/roi/controller/UserController.java
+++ src/main/java/com/roi/controller/UserController.java
@@ -2,22 +2,20 @@ package com.roi.controller;
 
 import com.roi.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.ui.ModelMap;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public ModelAndView student() {
+    public ModelAndView user() {
         return new ModelAndView("user", "command", new User());
     }
     @RequestMapping("/adduser.htm")
-    public String insert(HttpServletRequest req, User student)
+    public String insert(HttpServletRequest req, User user)
     {
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return "result";
